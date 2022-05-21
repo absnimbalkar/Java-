e543db00dcbacd4a22e4efef62ba328df69c909b
Abhishek Nimbalkar <65445090+absnimbalkar@users.noreply.github.com>
2022-05-21 15:20:11 +0530

    Add files via upload

---
+++ Database Programming/Donor.java
@@ -0,0 +1,60 @@
+import java.sql.*;
+import javax.swing.*;
+
+public class Donor
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			Class.forName("org.postgresql.Driver");
+			Connection c=DriverManager.getConnection("jdbc:postgresql://192.168.1.21/ty1", "ty1", "");
+			System.out.println("DB Connection successful");
+			
+			JFrame f=new JFrame("Donor Details");
+			
+
+			Statement s=c.createStatement();
+			ResultSet rs=s.executeQuery("select * from Donor");
+		
+			ResultSetMetaData rsmd=rs.getMetaData();
+			int cols=rsmd.getColumnCount();
+			int rows=0;
+			while(rs.next())
+			{
+				rows++;
+			}
+			rows++;
+			System.out.println("Rows= "+rows+" Columns="+cols);
+			
+			
+			JTable t=new JTable(rows, cols);
+			for(int i=1; i<=cols; i++)
+			{
+				t.setValueAt(rsmd.getColumnName(i), 0, i-1);
+			}
+			rs.close();
+			rs=s.executeQuery("select * from Donor");
+			
+			
+			int row=1;
+			while(rs.next())
+			{
+				t.setValueAt(new Integer(rs.getInt(1)), row, 0);
+				t.setValueAt(rs.getString(2), row, 1);
+				t.setValueAt(rs.getString(3), row, 2);
+				row++;
+			}
+			f.add(t);
+			f.setVisible(true);
+			f.setSize(400, 400);
+			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();	
+		}
+	}	
+	
+}
